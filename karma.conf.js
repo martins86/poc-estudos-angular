039268f39e1abe01b8cc8b1fc03d754c86fcb664
Thiago Martins de Freitas <martins86@gmail.com>
2021-04-30 22:55:07 -0300

    add Husky + scripts + config karma

--- karma.conf.js
+++ karma.conf.js
@@ -21,13 +21,28 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/poc-estudos-angular"),
-      subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      dir: "coverage",
+      reporters: [
+        { type: "html", subdir: "report-html" },
+        { type: "lcov", subdir: "report-lcov" },
+      ],
+      fixWebpackSourcePaths: true,
+    },
+    thresholds: {
+      emitWarning: false,
+      global: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
+      each: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
@@ -37,5 +52,15 @@ module.exports = function (config) {
     browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--headless", "--no-sandbox", "--remote-debugging-port=9222"],
+      },
+    },
+    browserDisconnectTolerance: 8,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 20000,
+    captureTimeout: 210000,
   });
 };
