88c36fa3b9c3b96d0b24083cdbe24d99fe4a0f37
Thiago Martins de Freitas <martins86@gmail.com>
2021-05-06 21:58:03 -0300

    add demo list

---
+++ src/app/pages/demos/demo-list/demo-list-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DemoListComponent } from './demo-list.component';
+
+const routes: Routes = [{ path: '', component: DemoListComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DemoListRoutingModule {}
